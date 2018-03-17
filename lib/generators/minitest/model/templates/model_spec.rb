fd4d7965ecb5d25e39a73898601d950f91998dc7
schadenfred <fred.schoeneman@gmail.com>
2018-03-16 20:54:50 -0700

    changed template for models

--- lib/generators/minitest/model/templates/model_spec.rb
+++ lib/generators/minitest/model/templates/model_spec.rb
@@ -2,10 +2,37 @@ require "test_helper"
 
 <% module_namespacing do -%>
 describe <%= class_name %> do
+
+  describe "db" do
+
+    specify "columns & types" do
+
+    end
+
+    specify "indexes" do
+
+    end
+
+  end
+
+  describe "associations" do
+    specify "belongs to" do
+
+    end
+
+    specify "has many" do
+
+    end
+  end
+
+  subject { <%= class_name %> }
+
   let(:<%= file_name %>) { <%= class_name %>.new }
 
   it "must be valid" do
     value(<%= file_name %>).must_be :valid?
   end
+
+
 end
 <% end -%>
