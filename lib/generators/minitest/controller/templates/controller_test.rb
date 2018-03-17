d7984060354062bda138bac999eb48e8291e1f42
schadenfred <fred.schoeneman@gmail.com>
2018-03-16 21:26:02 -0700

    added given fixture

--- lib/generators/minitest/controller/templates/controller_test.rb
+++ lib/generators/minitest/controller/templates/controller_test.rb
@@ -2,6 +2,8 @@ require "test_helper"
 
 <% module_namespacing do -%>
 class <%= class_name %>ControllerTest < ActionDispatch::IntegrationTest
+Given(:<%= class_name.to_s.downcase %>) { <%= class_name.classify.constantize.table_name %>.new }
+
 <% if mountable_engine? -%>
   include Engine.routes.url_helpers
 
