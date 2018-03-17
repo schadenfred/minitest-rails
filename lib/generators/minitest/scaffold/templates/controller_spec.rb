71a228dada7ea6812963794ebc79812138e51be5
schadenfred <fred.schoeneman@gmail.com>
2018-03-16 21:51:37 -0700

    added spacing

--- lib/generators/minitest/scaffold/templates/controller_spec.rb
+++ lib/generators/minitest/scaffold/templates/controller_spec.rb
@@ -16,33 +16,44 @@ describe <%= controller_class_name %>Controller do
   end
 
   describe "#new" do
+
     Given { get <%= new_helper %> }
+
     Then { value(response).must_be :success? }
   end
 
   describe "#create" do
+
     Given(:request) { post <%= index_helper %>_url, params: { <%= "#{singular_table_name}: { #{attributes_hash} }" %> } }
+
     Then { expect { request }.must_change "<%= class_name %>.count" }
 
-    must_redirect_to <%= singular_table_name %>_path(<%= class_name %>.last)
+    And { must_redirect_to <%= singular_table_name %>_path(<%= class_name %>.last) }
   end
 
   describe "#show" do
+
     Given { get <%= show_helper %> }
+
     Then { value(response).must_be :success? }
   end
 
   describe "#edit" do
+
     Given { get <%= edit_helper %> }
+
     Then { value(response).must_be :success? }
   end
 
   describe "#update" do
+
     Given { patch <%= show_helper %>, params: { <%= "#{singular_table_name}: { #{attributes_hash} }" %> } }
+
     Then { must_redirect_to <%= singular_table_name %>_path(<%= "#{singular_table_name}" %>) }
   end
 
   describe "#destroy" do
+
     Then { expect {
       delete <%= show_helper %>
     }.must_change "<%= class_name %>.count", -1}
